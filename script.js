1acf81a72d9f139c3c65e02efd25a46bb703a7e3
EzzEM99 <ezequiel.marmori@hotmail.com>
2024-07-07 23:04:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = '¡Te AMO';
+    document.getElementById('messageContainer').innerHTML = '¡Te AMO!';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
